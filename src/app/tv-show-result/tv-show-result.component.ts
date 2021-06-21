e4df3b31d2e636dda23f6e16b76399143b6fa6c0
rupalidashore <rupali.dashore@gmail.com>
2021-06-20 17:49:17 -0700

    ignore environment variable

--- src/app/tv-show-result/tv-show-result.component.ts
+++ src/app/tv-show-result/tv-show-result.component.ts
@@ -9,16 +9,7 @@ import { ITvShowApp } from '../i-tv-show-app';
 export class TvShowResultComponent implements OnInit {
   tvShow:ITvShowApp;   //create a variable tvShow
   constructor() { 
-    this.tvShow={
-      name: "The Blacklist", //dummy data example
-      rating: 7,
-      airdate: new Date(),
-      creator: "Jon Bokenkamp",
-      genres: "Crime Tv Show",
-      cast: "James Spader,Megan Boone",
-       image: "",
-      description:"This show is exciting"
-    }
+    
   }
 
   ngOnInit(): void {
